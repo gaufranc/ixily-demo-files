2d8a2e66359a3ea6d3bb59ccb53c343ef9d3c877
Gaspar Aufranc <gaspar.aufranc@gmail.com>
2022-03-09 17:19:14 -0300

    rebalance direct and trade ideas

---
+++ basics/rebalance-direct-object/index.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('index.html').pipe(res)
+})
+
+server.listen(process.env.PORT || 3008)
+
+console.log('Running on port 3008');
+
